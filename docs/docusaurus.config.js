21225209565ac3a6a7fcab72c194779e01b86e75
Leorium <contact@leorium.com>
2025-03-31 22:24:00 +0800

    docs: change baseUrl '/serdec' to '/serdec/'

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://docs.leorium.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/serdec',
+  baseUrl: '/serdec/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
