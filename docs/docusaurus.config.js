d3f7678f7facb69a23596db762d138e452af53e0
Leorium <contact@leorium.com>
2025-04-03 22:54:23 +0800

    docs: change tagline

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Serdec',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'A C library for serializing and deserializing C data.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
