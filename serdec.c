5bdc1e3d993e8b3b569c0ef956adb6fde8f20508
Leorium <contact@leorium.com>
2025-04-04 15:18:40 +0800

    chore: update serdec.c with MIT license header

--- serdec.c
+++ serdec.c
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025 Leorium <contact@leorium.com>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
 #include "serdec.h"
 
 JSON* json_create_object(void) {
