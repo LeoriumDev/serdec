930b308477d4ae5e7da019a5618c0ccbd9b0a9cb
Leorium <contact@leorium.com>
2025-04-25 15:47:17 +0800

    fix: remove old implmentation

--- serdec.c
+++ serdec.c
@@ -9,58 +9,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
 #include "serdec.h"
 
 #define INDENT "\x20\x20\x20\x20"
-
-JSON* json_create_object(void) {
-    JSON* object = malloc(sizeof(JSON));
-    object->next = object;
-    object->prev = object;
-    object->child = NULL;
-    return object;
-}
-
-void json_free_object(JSON* object) { free(object); }
-
-char* json_stringify(JSON* object) {
-    if (object != NULL)
-
-    return NULL;
-}
-
-bool json_write(JSON *object, char* filename) {
-    FILE* file;
-    file = fopen(filename, "w");
-    if (!file) {
-        perror(filename);
-        return false;
-    }
-
-    char* json_data = json_stringify(object);
-    if (!json_data) {
-        fclose(file);
-        return false;
-    }
-
-    fprintf(file, "%s", json_data);
-    fclose(file);
-    return true;
-}
-
-bool json_add_int(JSON *object, char *key, int value) {
-    object->type = JSON_INT;
-    object->value.int_val = value;
-    object->key = malloc(strlen(key) + 1);
-    strcpy(object->key, key);
-
-    JSON* tmp = json_create_object();
-    object->next = tmp;
-    object = object->next;
-    return true;
-}
