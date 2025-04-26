201b3d6dcf09c1bb9717443747c456dc0427db08
Leorium <contact@leorium.com>
2025-04-26 17:43:08 +0800

    feat(json): implement serdec_json_add_string to add string fields
    Add serdec_json_add_string, a function that inserts string fields
    into JSON objects safely. Handles memory allocation for both
    the key and string value, and uses serdec_json_list_add for insertion.

--- serdec.c
+++ serdec.c
@@ -227,7 +227,22 @@ bool serdec_json_add_float(serdec_json_t* object, const char* key, double value)
 }
 
 bool serdec_json_add_string(serdec_json_t* object, const char* key, const char* value) {
+    if (!object || object->json_type != SERDEC_JSON_OBJECT || !object->value.children)
+        return false;
 
+    serdec_json_t* new_node = serdec_json_new_string(value);
+    if (!new_node)
+        return false;
+
+    new_node->key = malloc(strlen(key) + 1);
+    if (!new_node->key) {
+        free(new_node);
+        return false;
+    }
+
+    strcpy(new_node->key, key);
+
+    return serdec_json_list_add(object, new_node);
 }
 
 bool serdec_json_add_array(serdec_json_t* object, const char* key, serdec_json_array_t* value) {
