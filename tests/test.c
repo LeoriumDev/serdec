84c200a3cd418c59896a90fd669c048dae75d8d3
Leorium <contact@leorium.com>
2025-05-13 16:47:56 +0800

    fix(json): re-added the test file for JSON serializing

---
+++ tests/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "serdec_json.h"
+
+int main(void) {
+    serdec_json_t* root = serdec_json_new_root();
+    serdec_json_add_int(root, "test", 123);
+    char* output = serdec_json_stringify(root);
+    
+    FILE* file = fopen("test.json", "w");
+    fprintf(file, "%s", output);
+    return EXIT_SUCCESS;
+}
